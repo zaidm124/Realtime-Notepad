156b393bd660bb455f64f468bd933ddb65626bf4
zaid bhimala <zaidm124@gmail.com>
2022-01-14 18:09:28 +0530

    turn server

--- realtime-notepad/frontend/src/components/VoiceChat/VoiceRoom.js
+++ realtime-notepad/frontend/src/components/VoiceChat/VoiceRoom.js
@@ -14,17 +14,36 @@ function VoiceRoom(props) {
   //   secure: process.env.REACT_APP_ENV === "PRODUCTION" ? true : false, // secure : false for http connection
   // });
 
-  let peer = new Peer({
-    host: "realtime-notepad-voice.herokuapp.com",
-    port: 9000,
-    debug: true,
-    secure: true,
+  var peer = new Peer({
+    key: "peerjs",
+    debug: 3,
     config: {
       iceServers: [
+        { url: "stun:stun.l.google.com:19302" }, // Pass in optional STUN and TURN server for maximum network compatibility
+        {
+          url: "turn:numb.viagenie.ca:3478",
+          credential: "muazkh",
+          username: "webrtc@live.com",
+        },
         {
           url: "turn:numb.viagenie.ca",
           credential: "muazkh",
           username: "webrtc@live.com",
+        },
+        {
+          url: "turn:numb.viagenie.ca:3478",
+          credential: "peerjsdemo",
+          username: "p.srikanta@gmail.com",
+        },
+        {
+          url: "turn:192.158.29.39:3478?transport=udp",
+          credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+          username: "28224511:1379330808",
+        },
+        {
+          url: "turn:192.158.29.39:3478?transport=tcp",
+          credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+          username: "28224511:1379330808",
         },
       ],
     },
